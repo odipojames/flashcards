84cf0a816ac75dda34df76fec704cd39db13c6fc
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-08 17:02:37 +0300

    urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.index,name='index'),
+    url(r'^card/(\d+)', views.card, name='card'),
+    url(r'^new/card$', views.new_card, name='new-card'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
