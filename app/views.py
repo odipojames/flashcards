baf2c31efd945fd23ba66e851629cb6456a294c2
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-08 17:03:07 +0300

    view functions that will show the  flashcrds app

--- app/views.py
+++ app/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import *
+from .models import *
+
+
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('flashcard-works')
+def index(request):
+
+    card = Card.objects.all()
+    print(Card) 
+    return render(request,'index.html', {"card":card})
+
+def card(request,card_id):
+    try:
+        card = Card.objects.get(id = card_id)
+    except Card.DoesNotExist:
+        raise Http404()
+    return render(request,"card.html", {"card":card})
+
+# @login_required(login_url='/accounts/login/')
+def new_card(request):
+    current_user = request.user
+    if request.method == 'card':
+        form = NewCardForm(request.card, request.FILES)
+        if form.is_valid():
+            card = form.save(commit=False)
+            card.editor = current_user
+            card.save()
+        return redirect('index')
+
+    else:
+        form = NewCardForm()
+    return render(request, 'new_card.html', {"form": form}) 
+
